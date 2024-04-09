848f74eac6058c696b781cb89e4a77b751631548
tariq7299 <tr.sar77an78@gmail.com>
2024-04-09 21:39:15 +0200

    Finished creating app that servers index.html page at localhost

--- BasicInformationalSite/index.js
+++ BasicInformationalSite/index.js
@@ -1,13 +1,25 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            console.error("Error")
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+        
+        })
+
 });
+
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
